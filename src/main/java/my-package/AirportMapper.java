b63d78bed1f0031d01abf90690a70ecbd509cb56
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 02:38:30 +0300

    Scripted auto-commit on change (2021-12-21 02:38:30) by gitwatch.sh

--- src/main/java/my-package/AirportMapper.java
+++ src/main/java/my-package/AirportMapper.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, FlightWritable> {
 
-
+    @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
 
