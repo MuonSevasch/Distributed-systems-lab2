c0ba2be0b2522bd42d113423be99704822c43fb8
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 04:45:02 +0300

    Scripted auto-commit on change (2021-12-21 04:45:02) by gitwatch.sh

--- src/main/java/my-package/FlightsJoinApp.java
+++ src/main/java/my-package/FlightsJoinApp.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
-
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
