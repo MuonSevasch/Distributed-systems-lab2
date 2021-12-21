fb0c404be39e9d31c5570f0c727b9cb5a41888f5
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 03:33:02 +0300

    Scripted auto-commit on change (2021-12-21 03:33:02) by gitwatch.sh

--- src/main/java/my-package/StatisticCounter.java
+++ src/main/java/my-package/StatisticCounter.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.io.IntWritable;
 import java.io.IOException;
 
 public class StatisticCounter extends Reducer<bmstu.ru.AirportWritableComparable, FlightWritable, IntWritable, Text> {
-
+    private static final String format = "Airport:%s  AVG_DELAY:%f, MIN_DELAY %f, MAX_DELAY:%f, NUM_FLIGHTS:%d";
 
 
 
