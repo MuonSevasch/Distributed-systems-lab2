26f79b4721648812b895ca3eae9f58e5fb7d5702
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 03:32:10 +0300

    Scripted auto-commit on change (2021-12-21 03:32:10) by gitwatch.sh

--- src/main/java/my-package/FlightWritable.java
+++ src/main/java/my-package/FlightWritable.java
@@ -3,7 +3,7 @@ package bmstu.ru;
 import org.apache.hadoop.io.Writable;
 
 import java.io.DataInput;
-
+import java.io.DataOutput;
 import java.io.IOException;
 
 public class FlightWritable implements Writable {
