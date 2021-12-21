e2b8d213a47e37a8c1e3fce4a2dae5384b21ecaf
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 03:13:15 +0300

    Scripted auto-commit on change (2021-12-21 03:13:15) by gitwatch.sh

--- src/main/java/my-package/FlightsJoinApp.java
+++ src/main/java/my-package/FlightsJoinApp.java
@@ -32,7 +32,7 @@ public class FlightsJoinApp {
         job.setMapOutputKeyClass(AirportWritableComparable.class);
         job.setMapOutputValueClass(FlightWritable.class);
 
-
+        job.setGroupingComparatorClass(FlightComparator.class);
 
 
 
