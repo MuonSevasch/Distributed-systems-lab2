0e77ad9c65cd354b32250e998df7bc93e7e75c38
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 03:52:58 +0300

    Scripted auto-commit on change (2021-12-21 03:52:58) by gitwatch.sh

--- src/main/java/my-package/FlightPartitioner.java
+++ src/main/java/my-package/FlightPartitioner.java
@@ -5,6 +5,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class FlightPartitioner extends Partitioner<AirportWritableComparable, FlightWritable> {
     @Override
     public int getPartition(AirportWritableComparable key, FlightWritable val, int i) {
-
+        return key.getId() % i;
     }
 }
