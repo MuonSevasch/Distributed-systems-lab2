4162152f59723dfcbdf331b4fa47b96814146f6b
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 03:19:52 +0300

    Scripted auto-commit on change (2021-12-21 03:19:52) by gitwatch.sh

--- src/main/java/my-package/FlightsMapper.java
+++ src/main/java/my-package/FlightsMapper.java
@@ -14,7 +14,7 @@ public class FlightsMapper extends Mapper<LongWritable, Text, bmstu.ru.AirportWr
 
 
             context.write(new AirportWritableComparable(data.getAirportId(), "", Type.FLIGHT),
-
+                    new FlightWritable("", data.getDelayTime(), Type.FLIGHT));
 
 
 }
