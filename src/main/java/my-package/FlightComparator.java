ff0af4a23f37747eac2b054b03a6af0ae1225a0b
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 03:02:47 +0300

    Scripted auto-commit on change (2021-12-21 03:02:47) by gitwatch.sh

--- src/main/java/my-package/FlightComparator.java
+++ src/main/java/my-package/FlightComparator.java
@@ -1,15 +1,15 @@
 
 
+import org.apache.hadoop.io.WritableComparable;
+
+
+public class FlightComparator extends WritableComparator {
+
+        super(AirportWritableComparable.class, true);
 
 
 
 
+        return ((AirportWritableComparable)a).getId() - ((AirportWritableComparable)b).getId();
 
-
-
-
-
-
-
-
-
+}
