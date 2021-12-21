61843ca10f9e349edf9d83b28ea4199d526961c8
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 03:07:24 +0300

    Scripted auto-commit on change (2021-12-21 03:07:24) by gitwatch.sh

--- src/main/java/my-package/FlightPartitioner.java
+++ src/main/java/my-package/FlightPartitioner.java
@@ -2,9 +2,9 @@
 
 
 
+public class FlightPartitioner extends Partitioner<AirportWritableComparable, FlightWritable> {
 
+    public int getPartition(AirportWritableComparable key, FlightWritable val, int i) {
+        return key.getId() % i;
 
-
-
-
-
+}
