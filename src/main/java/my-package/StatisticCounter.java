a501816e6b97e8545f1bba8b0d3b7186c9cbdc11
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 03:33:26 +0300

    Scripted auto-commit on change (2021-12-21 03:33:26) by gitwatch.sh

--- src/main/java/my-package/StatisticCounter.java
+++ src/main/java/my-package/StatisticCounter.java
@@ -11,7 +11,7 @@ public class StatisticCounter extends Reducer<bmstu.ru.AirportWritableComparable
     private static final String format = "Airport:%s  AVG_DELAY:%f, MIN_DELAY %f, MAX_DELAY:%f, NUM_FLIGHTS:%d";
 
 
-
+    protected void reduce(bmstu.ru.AirportWritableComparable key, Iterable<FlightWritable> values, Context context) throws IOException, InterruptedException {
 
 
 
